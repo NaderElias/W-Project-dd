ca45084dd3d0df851b006fa257e75bff6b4fafc3
Malak <malak.t5050@gmail.com>
2023-11-23 10:57:27 +0200

    Update workFlow.js

--- Backend/Models/workFlow.js
+++ Backend/Models/workFlow.js
@@ -5,6 +5,23 @@ const workFlowSchema = mongoose.Schema({
         type: String,
         required: true
       },
+    subCategory: {
+		type: String,
+		enum: [
+			"Desktops",
+			"Laptops",
+			"Printers",
+			"Servers",
+			"Networking equipment",
+			"Operating system",
+			"Application software",
+			"Custom software",
+			"Integration issues",
+			"Email issues",
+			"Internet connection problems",
+			"Website errors",
+		],
+	},
       workflow: [String]
 });
 
