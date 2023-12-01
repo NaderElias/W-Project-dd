a835f5c5e51e466bdd17e9ebf1fc77ec924af1a9
Malak <malak.t5050@gmail.com>
2023-12-01 12:47:55 +0200

    Update tickets.js

--- Backend/Routes/tickets.js
+++ Backend/Routes/tickets.js
@@ -1,5 +1,5 @@
 //user: create ticket
 //assign tickets (auto api not dependant on users)
 //agent/manager: get all tickets
-//agent: update ticket(close ticket is in here)
+//agent: update ticket(close ticket is in here then send email to users)
 //agent: update workflow
