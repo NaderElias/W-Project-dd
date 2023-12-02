b6cc6c8584a2218e042698ab6ed271513d53af25
NaderElias <103064354+NaderElias@users.noreply.github.com>
2023-12-02 20:57:40 +0200

    ger all and update fix update remains to be seen about the mangaerid update and authority over other managers

--- Backend/Controller/reportsController.js
+++ Backend/Controller/reportsController.js
@@ -37,8 +37,23 @@ const reportController = {
 	getAllReports: async (req, res) => {
 		try {
 			//getting all reports and outputting them
-			const allReports = await reportModel.find();
-			res.status(200).json({ reportsAnalytics : allReports });
+			const bod= req.body;
+			console.log(bod.ticketId);
+			if (bod.ticketId) {
+				const particReport = await reportModel.find({ticketId:bod.ticketId});
+				if(!particReport.ticketId){console.log("1");res.status(404).json({message: 'no report exists for thid ticket'})}
+				//console.log(particReport.managerId)
+				console.log("particular");
+				res.status(200).json({ reportsAnalytics : particReport });
+			}
+			else {
+				const allReports = await reportModel.find();
+				if(!allReports){console.log("no db");res.status(404).json({message: 'no reports in the database'})}
+				//console.log(allReports.managerId)
+				console.log("all found");
+				res.status(200).json({ reportsAnalytics : allReports });
+			}
+			
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({ message: "server error" });
@@ -56,6 +71,13 @@ const reportController = {
 			report.resolutionTime = resolutionTime;
 			report.agentPerformance = agentPerformance;
 
+			//check if the report exists
+			if(!report.ticketId){
+				res
+				.status(404)
+				.json({ message: "report does not exist"});}
+			// check if the current manager is the same one that created it and if it caan be others should i update the managerID
+			//
 			// Save the updated report to the database
 			await report.save();
 			res
