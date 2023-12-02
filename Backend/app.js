1c5f24ab77fba467306ea9e7f360175a63b56ab6
ASKalkas <96704211+ASKalkas@users.noreply.github.com>
2023-12-02 14:37:50 +0200

    users done
    file Controller/userController
    file Routes/users

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,14 @@ const cookieParser=require('cookie-parser')
 const app = express();
 const mongoose = require("mongoose");
 //always comment what you don't user
-//const ticketRouter = require("./Routes/tickets");
+const ticketRouter = require("./Routes/tickets");
 const userRouter = require("./Routes/users");
 const authRouter = require("./Routes/auth");
-// const automationRouter = require("./Routes/automation");
-// const brandingRouter = require("./Routes/branding");
-// const chatRouter = require("./Routes/chats");
-// const knowledgeBaseRouter = require("./Routes/knowledgeBase");
-// const reportsRouter = require("./Routes/reports");
+const automationRouter = require("./Routes/automation");
+const brandingRouter = require("./Routes/branding");
+const chatRouter = require("./Routes/chats");
+const knowledgeBaseRouter = require("./Routes/knowledgeBase");
+const reportsRouter = require("./Routes/reports");
 
 require('dotenv').config();
 const authenticationMiddleware = require("./Middleware/authenticationMiddleware");
@@ -36,13 +36,13 @@ app.use(
 //always comment what you don't use
 app.use("/api", authRouter);
 app.use(authenticationMiddleware);
-// app.use("/api/tickets", ticketRouter);
+app.use("/api/tickets", ticketRouter);
 app.use("/api/users", userRouter);
-// app.use("/api/automation", automationRouter);
-// app.use("/api/branding", brandingRouter);
-// app.use("/api/chats", chatRouter);
-// app.use("/api/knowledgeBase", knowledgeBaseRouter);
-// app.use("/api/reports", reportsRouter);
+app.use("/api/automation", automationRouter);
+app.use("/api/branding", brandingRouter);
+app.use("/api/chats", chatRouter);
+app.use("/api/knowledgeBase", knowledgeBaseRouter);
+app.use("/api/reports", reportsRouter);
 
 const db_name = process.env.DB_NAME;
 const db_url = `${process.env.DB_URL}/${db_name}`;
