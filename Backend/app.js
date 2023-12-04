7532628e585f017a58c2b1c97435a40126e3997e
ASKalkas <96704211+ASKalkas@users.noreply.github.com>
2023-12-05 00:02:08 +0200

    finished backup & surprise
    added scheduler to delete all expired tokens every day

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const mongoose = require("mongoose");
-//always comment what you don't user
+const cron = require("node-cron");
+const { spawn } = require("child_process");
+const sessionModel = require("./Models/sessionModel");
+
+//always comment what you don't use
 const ticketRouter = require("./Routes/tickets");
 const userRouter = require("./Routes/users");
 const authRouter = require("./Routes/auth");
@@ -10,7 +14,7 @@ const automationRouter = require("./Routes/automation");
 const brandingRouter = require("./Routes/branding");
 const chatRouter = require("./Routes/chats");
 const knowledgeBaseRouter = require("./Routes/knowledgeBase");
-const reportsRouter = require("./Routes/reports");
+const reportsRouter = require("./Routes/reportsAnalytics");
 
 require("dotenv").config();
 const authenticationMiddleware = require("./Middleware/authenticationMiddleware");
@@ -19,21 +23,60 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	next();
 });
 
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
+	cors({
+		origin: process.env.ORIGIN,
+		methods: ["GET", "POST", "DELETE", "PUT"],
+		credentials: true,
+	})
 );
 
-//always comment what you don't use
+const db_name = process.env.DB_NAME;
+const db_url = `${process.env.DB_URL}/${db_name}`;
+const ARCHIVE = process.env.ARCHIVE;
+
+const connectionOptions = {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+};
+
+function backupMongoDB() {
+	const child = spawn("mongodump", [
+		`--archive=${ARCHIVE}`,
+		`--uri=${db_url}`,
+		"--gzip",
+	]);
+	child.stdout.on("data", (data) => {
+		console.log(`stdout: ${data}`);
+	});
+	child.stderr.on("data", (data) => {
+		console.error(`stderr: ${data}`);
+	});
+	child.on("error", (error) => {
+		console.error(`error: ${error.message}`);
+		child.on("exit", code, (signal) => {
+			if (code) {
+				console.log(`process exit with code ${code} and signal ${signal}`);
+			} else if (signal) {
+				console.log(`process killed with signal ${signal}`);
+			} else {
+				console.log("backup process complete");
+			}
+		});
+	});
+}
+
+cron.schedule("0 0 * * * *", () => {
+	backupMongoDB();
+});
+
+// always comment what you don't use
 app.use("/api", authRouter);
 app.use(authenticationMiddleware);
 app.use("/api/tickets", ticketRouter);
@@ -44,22 +87,21 @@ app.use("/api/chats", chatRouter);
 app.use("/api/knowledgeBase", knowledgeBaseRouter);
 app.use("/api/reports", reportsRouter);
 
-const db_name = process.env.DB_NAME;
-const db_url = `${process.env.DB_URL}/${db_name}`;
-
-const connectionOptions = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-
 mongoose
-  .connect(db_url, connectionOptions)
-  .then(() => console.log("mongoDB connected"))
-  .catch((e) => {
-    console.log(e);
-  });
+	.connect(db_url, connectionOptions)
+	.then(() => {
+		console.log("mongoDB connected");
+		cron.schedule("0 0 * * *", () => {
+			sessionModel
+				.deleteMany({ "timeStamps.expiredAt": { $lt: Date.now() } })
+				.then(() => console.log("deleted sessions"));
+		});
+	})
+	.catch((e) => {
+		console.log(e);
+	});
 
 app.use(function (req, res, next) {
-  return res.status(404).send("404");
+	return res.status(404).send("404");
 });
 app.listen(3000, () => console.log("server started"));
