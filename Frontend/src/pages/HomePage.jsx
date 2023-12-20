3485796aee45ee74f54014998374b2110b5d2e4f
MalakTarek <133979342+MalakTarek@users.noreply.github.com>
2023-12-20 12:33:46 +0200

    navbar in homepage

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,6 +4,18 @@ import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 let backend_url = 'http://localhost:3000/api';
 
+const NavigationBar = ({ onUserListClick, onBrandingClick }) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#f0f0f0", padding: "10px" }}>
+      <div>
+        <button onClick={onUserListClick}>User List</button>
+        <button onClick={onBrandingClick}>Branding</button>
+      </div>
+      {/* You can add more items or styling as needed */}
+    </div>
+  );
+};
+
 export default function HomePage() {
   const navigate = useNavigate();
   const [cookies, removeCookies] = useCookies(['token']);
@@ -40,13 +52,14 @@ export default function HomePage() {
 
   return (
     <>
+      <NavigationBar
+        onUserListClick={handleUserListClick}
+        onBrandingClick={handleBrandingClick}
+      />
       <h1 style={{ textAlign: "center", margin: "30px", color: "black" }}>
         Welcome {userName}
       </h1>
-      <div style={{ textAlign: "center" }}>
-        <button onClick={handleUserListClick}>Go to User List</button>
-        <button onClick={handleBrandingClick}>Go to Branding</button>
-      </div>
+    
     </>
   );
 }
