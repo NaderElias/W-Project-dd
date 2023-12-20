ff4d4d278f58bf48521f10e8557736fa118a0e0a
MalakTarek <133979342+MalakTarek@users.noreply.github.com>
2023-12-20 09:55:45 +0200

    Admin create , view , assign done!!!
    role fe smoll no pretty thingy

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,12 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Router, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Homepage from "./pages/HomePage";
+import UserManagement from "./pages/UserList";
 
 function App() {
   return (
     <Routes>
           <Route path="/" element={<Homepage />} />      
           <Route path="/login" element={<Login  />} />
+          <Route path="/userlist" element={<UserManagement />} />
     </Routes>
   );
 }
