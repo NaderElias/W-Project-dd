fea08eb6042a9d132c8c69d99a5437b43c57fe0a
MalakTarek <133979342+MalakTarek@users.noreply.github.com>
2023-12-20 10:02:01 +0200

    button to connect everything<3

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,30 +1,26 @@
 import { useEffect, useState } from "react";
-//import AppNavBar from "../components/navbar";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-//require("dotenv").config();
 let backend_url = 'http://localhost:3000/api';
 
 export default function HomePage() {
   const navigate = useNavigate();
   const [cookies, removeCookies] = useCookies(['token']);
   const [userName, setUserName] = useState("");
-  // useeffect to fetch username
+
   useEffect(() => {
     async function fetchData() {
       try {
         if (!cookies.token) {
           navigate("/login");
+        } else {
+          const uid = localStorage.getItem("userId");
+          const response = await axios.get(`${backend_url}/users/get-profile?_id=${uid}`, {
+            withCredentials: true,
+          });
+          setUserName(response.data.user.username);
         }
-        else{
-        const uid = localStorage.getItem("userId");
-
-        const response = await axios.get(`${backend_url}/users/get-profile?_id=${uid}`, {
-          withCredentials: true,
-        });
-        setUserName(response.data.user.username);
-    }
       } catch (error) {
         console.log("error");
         console.log(error);
@@ -33,11 +29,19 @@ export default function HomePage() {
 
     fetchData();
   }, [cookies, navigate]);
+
+  const handleUserListClick = () => {
+    navigate("/userlist");
+  };
+
   return (
     <>
-      <h1 style={{ textAlign: "center", margin: "30px",color:'black' }}>
+      <h1 style={{ textAlign: "center", margin: "30px", color: "black" }}>
         Welcome {userName}
       </h1>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={handleUserListClick}>Go to User List</button>
+      </div>
     </>
   );
 }
