0770bf4911757715ea1a0445079f59c757a30e99
ASKalkas <96704211+ASKalkas@users.noreply.github.com>
2023-12-23 00:16:38 +0200

    fix shape

--- Frontend/src/components/TicketCard.jsx
+++ Frontend/src/components/TicketCard.jsx
@@ -1,39 +1,47 @@
+// src/components/TicketCard.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import '../styles/TicketCard.css';
 
-const TicketCard = ({ticketKey, ticket }) => {
+const TicketCard = ({ ticketKey, ticket }) => {
   const [newRating, setNewRating] = useState(1);
 
   const handleRatingChange = () => {
-    // Add logic to update the ticket rating via backend API
-    axios.put(`http://localhost:3000/api/tickets/update-Rating?_id=${ticketKey}`,{ rating: newRating },{ withCredentials: true })
+    axios.put(`http://localhost:3000/api/tickets/update-Rating?_id=${ticketKey}`, { rating: newRating }, { withCredentials: true })
       .then(response => {
-        response.data
+        console.log(response.data);
       })
       .catch(error => console.error('Error updating rating:', error));
   };
 
   return (
     <div className="ticket-card">
-    <h3>{ticket.title}</h3>
-    <p>{ticket.description}</p>
-    <p>Rating: {ticket.rating}</p>
-    <label htmlFor={`ratingDropdown-${ticket._id}`}>Select Rating:</label>
-    <select
-      id={`ratingDropdown-${ticket._id}`}
-      value={newRating}
-      onChange={(e) => setNewRating(e.target.value)}
-    >
-      {[1, 2, 3, 4, 5].map(value => (
-        <option key={value} value={value}>{value}</option>
-      ))}
-    </select>
-
-    <button onClick={handleRatingChange} className="change-rating-button">
-      Change Rating
-    </button>
-  </div>
+      <h3>{ticket.title}</h3>
+      <p>{ticket.description}</p>
+      <p className="rating">Rating: {ticket.rating}</p>
+      <div className="rating-section">
+        <label htmlFor={`ratingDropdown-${ticket._id}`}>Select Rating:</label>
+        <select
+          id={`ratingDropdown-${ticket._id}`}
+          value={newRating}
+          onChange={(e) => setNewRating(e.target.value)}
+        >
+          {[1, 2, 3, 4, 5].map(value => (
+            <option key={value} value={value}>{value}</option>
+          ))}
+        </select>
+        <button onClick={handleRatingChange} className="change-rating-button">
+          Change Rating
+        </button>
+      </div>
+    </div>
   );
+};
+
+TicketCard.propTypes = {
+  ticketKey: PropTypes.string.isRequired,
+  ticket: PropTypes.object.isRequired,
 };
 
 export default TicketCard;
--- Frontend/src/pages/Tickets.jsx
+++ Frontend/src/pages/Tickets.jsx
@@ -7,189 +7,213 @@ import TicketCard from "../components/TicketCard";
 Modal.setAppElement("#root"); // Set the root element for the modal
 
 function Tickets() {
-  const [tickets, setTickets] = useState([]);
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [newTicket, setNewTicket] = useState({ title: "", description: "",category:"",subCategory:"",priority:""});
+	const [tickets, setTickets] = useState([]);
+	const [isModalOpen, setModalOpen] = useState(false);
+	const [newTicket, setNewTicket] = useState({
+		title: "",
+		description: "",
+		category: "",
+		subCategory: "",
+		priority: "",
+	});
 
-  useEffect(() => {
-    // Fetch tickets from backend API
-    if (localStorage.getItem("role") === "agent") {
-      axios
-        .get(
-          `http://localhost:3000/api/tickets/get-All-Tickets?userId=${localStorage.getItem(
-            "userId"
-          )}`,
-          {
-            withCredentials: true,
-          }
-        )
-        .then((response) => setTickets(response.data.tickets))
-        .catch((error) => console.error("Error fetching tickets:", error));
-    } else if (localStorage.getItem("role") === "user") {
+	useEffect(() => {
+		// Fetch tickets from backend API
+		if (localStorage.getItem("role") === "agent") {
+			axios
+				.get(
+					`http://localhost:3000/api/tickets/get-All-Tickets?userId=${localStorage.getItem(
+						"userId"
+					)}`,
+					{
+						withCredentials: true,
+					}
+				)
+				.then((response) => setTickets(response.data.tickets))
+				.catch((error) => console.error("Error fetching tickets:", error));
+		} else if (localStorage.getItem("role") === "user") {
+			axios
+				.get(
+					`http://localhost:3000/api/tickets/get-All-Tickets?userId=${localStorage.getItem(
+						"userId"
+					)}`,
+					{
+						withCredentials: true,
+					}
+				)
+				.then((response) => setTickets(response.data.tickets))
+				.catch((error) => console.error("Error fetching tickets:", error));
+		} else {
+			axios
+				.get("http://localhost:3000/api/tickets/get-All-Tickets", {
+					withCredentials: true,
+				})
+				.then((response) => setTickets(response.data.tickets))
+				.catch((error) => console.error("Error fetching tickets:", error));
+		}
+	}, []); // Empty dependency array ensures the effect runs once on mount
 
-      axios
-        .get(
-          `http://localhost:3000/api/tickets/get-All-Tickets?userId=${localStorage.getItem( "userId")}`,
-          {
-            withCredentials: true,
-          }
-        )
-        .then((response) => setTickets(response.data.tickets))
-        .catch((error) => console.error("Error fetching tickets:", error));}
-     else {
-      axios
-        .get("http://localhost:3000/api/tickets/get-All-Tickets", { withCredentials: true })
-        .then((response) => setTickets(response.data.tickets))
-        .catch((error) => console.error("Error fetching tickets:", error));
-    }
-    
-  }, []); // Empty dependency array ensures the effect runs once on mount
+	const openModal = () => {
+		setModalOpen(true);
+	};
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
+	const closeModal = () => {
+		setModalOpen(false);
+		setNewTicket({
+			title: "",
+			description: "",
+			category: "",
+			subCategory: "",
+			priority: "",
+		}); // Reset form fields
+	};
 
-  const closeModal = () => {
-    setModalOpen(false);
-    setNewTicket({ title: "", description: "",category:"",subCategory:"",priority:"" }); // Reset form fields
-  };
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setNewTicket((prevTicket) => ({ ...prevTicket, [name]: value }));
+	};
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewTicket((prevTicket) => ({ ...prevTicket, [name]: value }));
-  };
+	const createNewTicket = async () => {
+		console.log(newTicket);
+		const tick = await axios
+			.post(
+				`http://localhost:3000/api/tickets/create-Ticket?userId=${localStorage.getItem(
+					"userId"
+				)}`,
+				newTicket,
+				{ withCredentials: true }
+			)
+			.catch((error) => console.error("Error creating ticket:", error));
+		closeModal();
 
-  const createNewTicket  = async () => {
-   console.log(newTicket);
-   const tick= await axios
-    .post(`http://localhost:3000/api/tickets/create-Ticket?userId=${localStorage.getItem("userId")}`, newTicket, { withCredentials: true })
-    .catch((error) => console.error("Error creating ticket:", error));
-    closeModal();
+		// Close the modal after creating the ticket
+	};
 
-    // Close the modal after creating the ticket
-    
-  };
+	return (
+		<div className="Tickets">
+			<h1>Your Tickets</h1>
+			<div className="ticketContainer">
+				{tickets.length > 0 ? (
+					tickets.map((ticket) => (
+						<TicketCard key={ticket._id} ticket={ticket} />
+					))
+				) : (
+					<p>No tickets available.</p>
+				)}
+			</div>
+			<button className="newTicketButton" onClick={openModal}>
+				Create New Ticket
+			</button>
 
-  return (
-    <div className="Tickets">
-     <h1>Your Tickets</h1>
-<div className="ticketContainer">
-  
-  {tickets.length > 0 ? (
-    tickets.map((ticket) => (
-      <TicketCard ticketKey={ticket._id} ticket={ticket} />
-    ))
-  ) : (
-    <p>No tickets available.</p>
-  )}
+			{/* Modal for creating a new ticket */}
 
+			<Modal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				className="modal"
+				overlayClassName="overlay"
+			>
+				<div className="modal-content">
+					<h2>Create New Ticket</h2>
 
-      </div>
-      <button className="newTicketButton" onClick={openModal}>
-        Create New Ticket
-      </button>
+					<div className="form-group">
+						<label htmlFor="category">Category:</label>
+						<select
+							name="category"
+							value={newTicket.category}
+							onChange={handleInputChange}
+							className="select-dropdown"
+						>
+							<option value="software">Software</option>
+							<option value="hardware">Hardware</option>
+							<option value="network">Network</option>
+						</select>
+					</div>
 
-      {/* Modal for creating a new ticket */}
-      
-      <Modal
-  isOpen={isModalOpen}
-  onRequestClose={closeModal}
-  className="modal"
-  overlayClassName="overlay"
->
-  <div className="modal-content">
-    <h2>Create New Ticket</h2>
+					<div className="form-group">
+						<label htmlFor="subCategory">Sub category:</label>
+						<select
+							name="subCategory"
+							value={newTicket.subCategory}
+							onChange={handleInputChange}
+							className="select-dropdown"
+						>
+							<option value="Desktops">Desktops</option>
+							<option value="Laptops">Laptops</option>
+							<option value="Printers">Printers</option>
+							<option value="Servers">Servers</option>
+							<option value="Networking equipment">Networking equipment</option>
+							<option value="Operating system">Operating system</option>
+							<option value="Application software">Application software</option>
+							<option value="Custom software">Custom software</option>
+							<option value="Integration issues">Integration issues</option>
+							<option value="Email issues">Email issues</option>
+							<option value="Internet connection problems">
+								Internet connection problems
+							</option>
+							<option value="Website errors">Website errors</option>
+						</select>
+					</div>
 
-    <div className="form-group">
-      <label htmlFor="category">Category:</label>
-      <select
-        name="category"
-        value={newTicket.category}
-        onChange={handleInputChange}
-        className="select-dropdown"
-      >
-        <option value="software">Software</option>
-        <option value="hardware">Hardware</option>
-        <option value="network">Network</option>
-      </select>
-    </div>
+					<form>
+						<div className="form-group">
+							<label htmlFor="title">Title:</label>
+							<input
+								type="text"
+								id="title"
+								name="title"
+								value={newTicket.title}
+								onChange={handleInputChange}
+								className="input-field"
+							/>
+						</div>
 
-    <div className="form-group">
-      <label htmlFor="subCategory">Sub category:</label>
-      <select
-        name="subCategory"
-        value={newTicket.subCategory}
-        onChange={handleInputChange}
-        className="select-dropdown"
-      >
-        <option value="Desktops">Desktops</option>
-        <option value="Laptops">Laptops</option>
-        <option value="Printers">Printers</option>
-        <option value="Servers">Servers</option>
-        <option value="Networking equipment">Networking equipment</option>
-        <option value="Operating system">Operating system</option>
-        <option value="Application software">Application software</option>
-        <option value="Custom software">Custom software</option>
-        <option value="Integration issues">Integration issues</option>
-        <option value="Email issues">Email issues</option>
-        <option value="Internet connection problems">Internet connection problems</option>
-        <option value="Website errors">Website errors</option>
-      </select>
-    </div>
+						<div className="form-group">
+							<label htmlFor="description">Description:</label>
+							<textarea
+								id="description"
+								name="description"
+								value={newTicket.description}
+								onChange={handleInputChange}
+								className="textarea-field"
+							/>
+						</div>
 
-    <form>
-      <div className="form-group">
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={newTicket.title}
-          onChange={handleInputChange}
-          className="input-field"
-        />
-      </div>
+						<div className="form-group">
+							<label htmlFor="priority">Priority:</label>
+							<select
+								name="priority"
+								value={newTicket.priority}
+								onChange={handleInputChange}
+								className="select-dropdown"
+							>
+								<option value="high">High</option>
+								<option value="medium">Medium</option>
+								<option value="low">Low</option>
+							</select>
+						</div>
 
-      <div className="form-group">
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          name="description"
-          value={newTicket.description}
-          onChange={handleInputChange}
-          className="textarea-field"
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="priority">Priority:</label>
-        <select
-          name="priority"
-          value={newTicket.priority}
-          onChange={handleInputChange}
-          className="select-dropdown"
-        >
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-      </div>
-
-      <div className="button-group">
-        <button type="button" onClick={createNewTicket} className="create-button">
-          Create Ticket
-        </button>
-        <button type="button" onClick={closeModal} className="cancel-button">
-          Cancel
-        </button>
-      </div>
-    </form>
-  </div>
-</Modal>
-
-
-    </div>
-  );
+						<div className="button-group">
+							<button
+								type="button"
+								onClick={createNewTicket}
+								className="create-button"
+							>
+								Create Ticket
+							</button>
+							<button
+								type="button"
+								onClick={closeModal}
+								className="cancel-button"
+							>
+								Cancel
+							</button>
+						</div>
+					</form>
+				</div>
+			</Modal>
+		</div>
+	);
 }
 
 export default Tickets;
