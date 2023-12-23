b36696f17488f1d815683da2a00fe50c0276121c
ItachiKhalil <134069304+ItachiKhalil@users.noreply.github.com>
2023-12-24 01:49:31 +0200

    itachi 3amalha

--- Frontend/src/pages/Analytics.jsx
+++ Frontend/src/pages/Analytics.jsx
@@ -1,69 +1,95 @@
+import { PieChart, Pie, Cell, Legend } from "recharts";
 import React, { useEffect, useState } from "react";
-import { Bar, Doughnut } from "react-chartjs-2";
+import axios from "axios";
 
 const AnalyticsPage = () => {
-  const [analyticsData, setAnalyticsData] = useState({
-    issue: [],
-    statusPercent: [],
-    statusPercentfilter: [],
-    relation: [],
-  });
-
+  const [analyticsData, setAnalyticsData] = useState(null);
   useEffect(() => {
-    const fetchAnalyticsData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch("/api/getAnalytics"); // Update the API endpoint
-        const data = await response.json(); // Add await here
-        setAnalyticsData(data);
+        const response = await axios.get(
+          "http://localhost:3000/api/reports/get-Analytics",
+          { withCredentials: true }
+        );
+        setAnalyticsData(response.data);
       } catch (error) {
         console.error("Error fetching analytics data:", error);
       }
     };
 
-    fetchAnalyticsData();
+    fetchData();
   }, []); // Empty dependency array ensures that this effect runs once when the component mounts
 
-  const barChartData = {
-    labels: analyticsData.issue.map((item) => item._id), // Use analyticsData here
-    datasets: [
-      {
-        label: "Issue Count",
-        data: analyticsData.issue.map((item) => item.count), // Use analyticsData here
-        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-      },
-    ],
-  };
+  const data = [
+    { name: "Geeksforgeeks", students: 400 },
+    { name: "Technical scripter", students: 700 },
+    { name: "Geek-i-knack", students: 200 },
+    { name: "Geek-o-mania", students: 1000 },
+  ];
+  const colors = ["#36A2EB", "#FFCE56", "#FF6384"]; // Customize colors as needed
 
-  // Rest of the code remains unchanged
+  let issue;
+  let statusPercent;
+  let statusPercentFilter;
+  let relation;
+
+  for (const key in analyticsData) {
+    if (analyticsData.hasOwnProperty(key)) {
+      const value = analyticsData[key];
+
+      // Output key and value to console
+      console.log(`${key}:`, value);
+
+      // If the value is an array or object, you can further iterate through its elements
+      if (Array.isArray(value) || typeof value === "object") {
+        // Assign values to specific variables based on the key
+        switch (key) {
+          case "issue":
+            issue = value;
+            break;
+          case "statusPercent":
+            statusPercent = value;
+            break;
+          case "statusPercentfilter":
+            statusPercentFilter = value;
+            break;
+          case "relation":
+            relation = value;
+            break;
+          default:
+            break;
+        }
+
+        // Log each element of the array
+        for (const item of value) {
+          console.log(`${key} element:`, item);
+        }
+      }
+    }
+  }
+
+  // Now you can use the variables: issue, statusPercent, statusPercentFilter, and relation
+  console.log("issue:", issue);
+  console.log("statusPercent:", statusPercent);
+  console.log("statusPercentFilter:", statusPercentFilter);
+  console.log("relation:", relation);
 
   return (
-    <div>
-      <h1>Analytics Page</h1>
-
-      {/* Bar Chart */}
-      <div>
-        <h2>Issue Count</h2>
-        <Bar data={barChartData} />
-      </div>
-
-      {/* Doughnut Chart */}
-      <div>
-        <h2>Status Percentage</h2>
-        <Doughnut data={doughnutChartData} />
-      </div>
-
-      {/* Stacked Bar Chart */}
-      <div>
-        <h2>Status Percentage Filter</h2>
-        <Bar data={stackedBarChartData} />
-      </div>
-
-      {/* Grouped Bar Chart */}
-      <div>
-        <h2>Relation Percentage</h2>
-        <Bar data={groupedBarChartData} />
-      </div>
-    </div>
+    <PieChart width={700} height={700}>
+      <Pie
+        data={statusPercent}
+        dataKey="percentage"
+        nameKey="status"
+        outerRadius={250}
+        fill="#8884d8"
+        label
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+        ))}
+      </Pie>
+      <Legend />
+    </PieChart>
   );
 };
 
